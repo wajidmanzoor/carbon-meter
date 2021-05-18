5abfed024f894d27a47cced06c8fe8d9fd766140
wajidmanzoor <wajid.manzoor.1999@gmail.com>
2021-05-18 21:09:59 +0530

    hardware,cloud provider and countries added

---
+++ gui.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+import tkinter.ttk
+
+
+countries = {"Google Cloud": ['China ', 'Taiwan ', 'Japan ', 'India ', 'Singapore ', 'Australia ', 'Finland ', 'Belgium ', 'United Kingdom ', 'Germany ', 'Netherlands ', 'Switzerland ', 'Canada ', 'Brazil ', 'USA '],
+             "Amazon Web Service": ['USA ', 'China ', 'India ', 'Japan ', 'South Korea ', 'Singapore ', 'Australia ', 'Canada ', 'Germany ', 'Ireland ', 'United Kingdom ', 'France ', 'Sweden ', 'Brazil '],
+             "Azure": ['Hong Kong ', 'Singapore ', 'USA ', 'Ireland ', 'Netherlands ', 'Japan ', 'Brazil ', 'Australia ', 'India ', 'Canada ', 'United Kingdom ', 'South Korea ', 'France ', 'South Africa ']}
+hardware = ['RTX 2080 Ti', 'RTX 2080', 'GTX 1080 Ti', 'GTX 1080', 'AMD RX480', 'Titan ValueError(', 'Tesla V100-SXM2-32GB', 'Tesla V100-SXM2-16GB', 'Tesla P100', 'Tesla K40c', 'Tesla K80', 'TPU2', 'TPU3',
+            'Intel Xeon E5-2699', 'AGX Xavier', 'GTX TITAN X', 'TITAN X Pascal', 'T4', 'Titan RTX', 'RTX 8000', 'GTX 750', 'Quadro K6000', 'Quadro P6000', 'Tesla M40 24GB', 'Tesla V100-PCIE-16GB', 'Titan Xp']
